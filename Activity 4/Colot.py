19aee1e2328338a87cd48c3fdd03ecc20ad54ec7
crizel2006 <colotcrizel16@gmail.com>
2024-09-23 18:33:14 -0700

    Colot.py

---
+++ Activity 4/Colot.py
@@ -0,0 +1,15 @@
+# sampleText1 = "My name is {}. I love {}. I love playing {}. {}"
+# sampleText1a = sampleText1.format("Crizel", "Milktea", "Billiard", "Apple")
+# print(sampleText1a)
+
+
+
+# sampleText2 = "My name is {1}. I love {2}. I love playing {0}."
+# sampleText2a = sampleText2.format("COD", "Crizel", "Milktea", "Billiard", "Apple")
+# print(sampleText2a)
+
+
+
+# sampleText3 = "My name is {name}. I love {food}. I love playing {play}."
+# sampleText3a = sampleText3.format(food="Milktea", play="Billiard", name="Crizel")
+# print(sampleText3a)
